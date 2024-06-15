67e8cbdb9a08cc425edd02ad7bccf4cdee7be674
Kagiso Monageng <63680048+KagisoMonageng@users.noreply.github.com>
2024-06-15 11:36:30 +0200

    Update db-config.js

--- backend/config/db-config.js
+++ backend/config/db-config.js
@@ -1,6 +1,6 @@
 const pg = require('pg')
 require("dotenv").config();
-var client = new pg.Client(process.env.CONN); //Connection string here
+var client = new pg.Client('postgresql://postgres:postgres@13.48.44.148:5432/nextGen'); //Connection string here
 client.connect(function(err){
   if (err) {
     console.log("Database connection error");
