e886afeb75c9ce9edfbacb2c15563b92879f7bdb
KagisoMonageng <63680048+KagisoMonageng@users.noreply.github.com>
2024-08-02 06:49:06 +0200

    Auth Back end

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -1,15 +1,135 @@
-// Imports here
+const bcrypt = require("bcrypt");
+const db = require("../config/db-config");
+const jwt = require("jsonwebtoken");
+
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+      const data = await db.query("SELECT * FROM users WHERE email= $1", [
+        email,
+      ]); //Verifying if the user exists in the database
+      const user = data.rows[0];
+      if (data.rows.length === 0) {
+        res.status(400).json({
+          message: "User is not registered, Register first",
+        });
+      } else {
+        console.log(user.password)
+        bcrypt.compare(password, user.password, (err, result) => {
+          //Comparing the hashed password
+          if (err) {
+            res.status(500).json({
+              message: "Encryption failed",
+            });
+          } else if (result === true) {
+            //Checking if credentials match
+            const token = jwt.sign(
+              {
+                email: email,
+                id: user.id,
+                name: user.name,
+                surname: user.surname,
+                short_bio: user.short_bio,
+                profile_image: user.profile_image,
+                profile_banner: user.profile_banner
+              },
+              process.env.SECRET_KEY,{
+                  expiresIn:'24h',
+                  algorithm:'HS256'
+              }
+            );
+            res.status(200).json({
+              message: "Welcome back! "+user.name,
+              token: token
+            });
+          } else {
+            //Declaring the errors
+            if (result != true)
+              res.status(400).json({
+                message: "Incorect login credentials",
+              });
+          }
+        });
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({
+        message: "Database error occurred while signing in!", //Database connection error
+      });
+    }
+  };
 
 
-exports.login = async (req, res) => { 
+//Registration Function
+exports.register = async (req, res) => {
+  const { name, email, surname, password } = req.body;
 
-    res.status(200).json({ message: "Response here" });
-    // Login function in here
-}
+  const profile_image = "https://res.cloudinary.com/dkvrb3pye/image/upload/v1675240135/vecteezy_profile-user-icon-isolated-on-white-background-vector-eps10__a4gxpc.jpg";
+  const profile_banner = "https://res.cloudinary.com/dkvrb3pye/image/upload/v1675240135/vecteezy_profile-user-icon-isolated-on-white-background-vector-eps10__a4gxpc.jpg";
+  const last_login = new Date().getFullYear() + "-" + new Date().getMonth() + "-" + new Date().getDate();
+  const short_bio = 'Hey there, I am new on NextGen'
 
+  try {
+    const data = await db.query(`SELECT * FROM users WHERE email= $1;`, [
+      email,
+    ]); //Checking if user already exists
+    const arr = data.rows;
+    if (arr.length != 0) {
+      return res.status(400).json({
+        message: "Email already exists, Please log in",
+      });
+    } else {
+      bcrypt.hash(password, 10, (err, hash) => {
+        if (err)
+          res.status(err).json({
+            message: "Server error",
+          });
+        var flag = 1; //Declaring a flag
 
+        //Inserting data into the database
 
-exports.register = async (req, res) => { 
-    res.status(200).json({ message: "Response here" });
-    // Register function in here
-}
+        db.query(
+          `INSERT INTO Users ( name,surname,email, password,profile_image,profile_banner,last_login,short_bio) VALUES ($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *`,
+          [
+            name,surname,email, hash ,profile_image,profile_banner,last_login,short_bio
+          ],
+          (err,results) => {
+            if (err) {
+              flag = 0; //If user is not inserted is not inserted to database assigning flag as 0/false.
+              console.error(err);
+              return res.status(500).json({
+                message: "Database error",
+              });
+            } else {
+              flag = 1;
+              const token = jwt.sign(
+                //Signing a jwt token
+                {
+                    email: email,
+                    id: results.rows[0].id,
+                    name: name,
+                    surname: surname,
+                    short_bio: short_bio,
+                    profile_image: profile_image,
+                    profile_banner: profile_banner
+                  },
+                  process.env.SECRET_KEY,{
+                    expiresIn:'24h',
+                    algorithm:'HS256'
+                });
+              
+              res.status(201).json({message:'You are now registered',token:token});
+
+            }
+          }
+        );
+       
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Database error while registring user!", //Database connection error
+    });
+  }
+};
