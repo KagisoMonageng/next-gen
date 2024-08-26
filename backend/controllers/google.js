8d720cbcc967875a4580a7fb315a2b06ca087eaf
Kagiso Monageng <63680048+KagisoMonageng@users.noreply.github.com>
2024-08-26 16:45:34 +0200

    Update google.js

--- backend/controllers/google.js
+++ backend/controllers/google.js
@@ -2,12 +2,13 @@ const passport = require('passport');
 require("dotenv").config();
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const jwt = require('jsonwebtoken');
-
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_KEY = process.env.CLIENT_KEY;
 
 
 passport.use(new GoogleStrategy({
-  clientID: '295049434949-bdb1ue7dpca36ja02rocs3n5hbdg7lo0.apps.googleusercontent.com',
-  clientSecret: 'OCSPX-4eRar_h_LF-H7MJwDSwbVr90tzny',
+  clientID: CLIENT_ID,
+  clientSecret: CLIENT_KEY,
   callbackURL: 'http://localhost:3000/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
   // Logic to handle user data
